6992120fa6661a1baf14a41ebaed76ba031dd616
Murali Kashaboina <kbmuralidhar@gmail.com>
2014-04-27 16:22:38 -0500

    Fixed setting Local to C equivalent of NULL in enqueue_nd_range function

--- c_src/cl_nif.c
+++ c_src/cl_nif.c
@@ -4103,13 +4103,11 @@ static ERL_NIF_TERM ecl_enqueue_nd_range_kernel(ErlNifEnv* env, int argc,
     if (!get_bool(env, argv[5], &want_event))
 	return enif_make_badarg(env);
 
-    if (work_dim == 0)
-	{
+    if (work_dim == 0){
 		return enif_make_badarg(env);
 	}
 	
-	if( temp_dim > 0 && work_dim != temp_dim )
-	{
+	if( temp_dim > 0 && work_dim != temp_dim ){
 		return enif_make_badarg(env);
 	}
 
